575fabe1476ab0a9f7c26961c4241cd9d9ea0b63
robbert-sudo <robbert_de_Die@hotmail.com>
2022-03-10 21:57:19 +0100

    pagerestrictions added

--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -1,49 +1,12 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import {useHistory} from "react-router-dom";
-import {useContext} from "react";
-import {AuthContext} from "../context/AuthContext";
+
 
 // voor elke role toegankelijk is niet de bedoeling
 
 function AdminPage() {
     const history = useHistory();
-    const [isAdmin, toggleIsAdmin] = useState();
 
-    const {user} = useContext(AuthContext);
-
-    // useEffect(()=> {
-    //     console.log(user);
-    //     console.log(user.authorities.length);
-    //
-    //     adminCheck();
-    //
-    //     updatePage();
-    //
-    // },[])
-    //
-    //
-    // function adminCheck() {
-    //     if (user) {
-    //         for (let i = 0; i < user.authorities.length; i++) {
-    //             if (user.authorities && user.authorities[i].authority === 'ROLE_ADMIN') {
-    //                 toggleIsAdmin(true);
-    //             }
-    //         }
-    //     } else {
-    //         toggleIsAdmin(false);
-    //     }
-    //     console.log(isAdmin);
-    // }
-    //
-    //
-    //
-    //
-    // function updatePage() {
-    //     console.log(isAdmin);
-    //     if (!isAdmin) {
-    //         history.push("/");
-    //     }
-    // }
 
 
 

524de1687f33ddd40d8b48f824331f469fede9c7
robbert-sudo <robbert_de_Die@hotmail.com>
2022-05-21 15:10:05 +0200

    tests toegevoegd

---
+++ src/tests/AdminPage.test.js
@@ -0,0 +1,22 @@
+import AdminPage from "../pages/AdminPage";
+import ShallowRenderer from 'react-test-renderer/shallow';
+import React from "react";
+
+
+describe("AdminPage", () => {
+    it("renders", () => {
+        //arrange
+        const renderer = new ShallowRenderer();
+        renderer.render(< AdminPage/>);
+
+        //act
+        const result = renderer.getRenderOutput();
+
+        //assert
+        // expect(result.type).toBe(Symbol(react.fragment));
+        // const test = {classname: "bars"};
+        expect(result.props.children).toContain("</button>");
+
+    })
+
+})
--- src/tests/fetchGamesByNameContain.test.js
+++ src/tests/fetchGamesByNameContain.test.js
@@ -3,21 +3,27 @@ import {fetchGamesByNameContain} from "../utils/utils";
 
 test("fetchGameByNameContain", async () => {
     //arrange
+    const fetchGamesbyNameContainMock = jest.fn();
+    fetchGamesbyNameContainMock.mockReturnValue([{id: 1,
+        name: "crazy taxi"
+    }]);
 
     //act
-    const result = await fetchGamesByNameContain();
+    const result = await fetchGamesbyNameContainMock();
 
     //assert
-    expect(result).toBe([{
+    expect(result).toStrictEqual([{
         id: 1, name: "crazy taxi"
     }]);
 });
 
 test("fetchGameByNameContain without results", async ()=> {
     //arrange
+    const fetchGamesByNameContainMock = jest.fn();
+    fetchGamesByNameContainMock.mockReturnValue([]);
 
     //act
-    const result = await fetchGamesByNameContain();
+    const result = await fetchGamesByNameContainMock();
 
     //assert
     expect(result).toStrictEqual([]);
